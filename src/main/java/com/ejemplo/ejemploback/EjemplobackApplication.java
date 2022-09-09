4f12e8f74644dcc7ddfc000bed58aecd02e00929
Sebastian Bejarano <sebastian_bejarano@epam.com>
2022-09-08 19:29:24 -0500

    Inicialización de repositorio

---
+++ src/main/java/com/ejemplo/ejemploback/EjemplobackApplication.java
@@ -0,0 +1,13 @@
+package com.ejemplo.ejemploback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjemplobackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjemplobackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ejemplo/ejemploback/EjemplobackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ejemplo.ejemploback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjemplobackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
